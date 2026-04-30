08ff6c023bae05e5b282e25310433cd76b409ad9
wendyle511 <wle7@uci.edu>
2026-04-30 09:00:16 -0700

    add autogenerate report

--- launch.py
+++ launch.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 from utils.server_registration import get_cache_server
 from utils.config import Config
 from crawler import Crawler
-
+from scraper import generate_report
 
 def main(config_file, restart):
     cparser = ConfigParser()
@@ -13,6 +13,8 @@ def main(config_file, restart):
     config.cache_server = get_cache_server(config, restart)
     crawler = Crawler(config, restart)
     crawler.start()
+    generate_report()
+    print("Report generated")
 
 
 if __name__ == "__main__":
